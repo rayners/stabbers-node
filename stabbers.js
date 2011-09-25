f2e840d98db2fb240615efcd6dbf608402cf9560
David Raynes <rayners@gmail.com>
2011-09-24 23:47:40 -0400

    Fixup regular expressions and jerk usage

--- stabbers.js
+++ stabbers.js
@@ -7,10 +7,6 @@ var server = process.env.IRC_SERVER || 'irc.freenode.net';
 var nick   = process.env.IRC_NICK   || 'stabbers-node';
 var prefix = '.';
 
-/*var client = new irc.Client(server, nick, {
-    channels: channels,
-});*/
-
 var actions = {
     "stab": "stabs",
     "slap": "slaps",
@@ -25,8 +21,8 @@ var acronyms = {
 for (action in actions) {
     jerk( function( j ) {
 	var verb = actions[action];
-	j.watch_for(new RegExp("^\\." + action + '(?: (.*))$'), function(message) {
-	    var predicate = message.match_data[1] || message.user;
+	j.watch_for(new RegExp("^\\." + action + '( (.*))?$'), function(message) {
+	    var predicate = message.match_data[2] || message.user;
 	    var act = '';
 	    if (typeof(verb) === 'function') {
 		act = verb(message.user, predicate);
@@ -41,39 +37,11 @@ for (action in actions) {
 
 for (acronym in acronyms) {
     jerk( function( j ) {
+	var a = acronyms[acronym];
 	j.watch_for(new RegExp("^\\." + acronym + "$"), function(message) {
-	    message.say(acronyms[acronym]);
+	    message.say(a);
 	});
     });
 }
 
-jerk().connect({ server: server, nick: nick, channels: channels });
-
-/*client.addListener('message', function (from, to, message) {
-    if (message.match(/^\./)) {
-	// it's a command!
-	var bits = message.split(' ', 2);
-
-	// nix the '.' at the beginning
-	var command = bits[0].substring(1);
-	var predicate = bits[1] || from;
-
-	// is it an action?
-	if (command in actions) {
-            var action = '';
-	    if (typeof(actions[command]) === 'function') {
-		action += actions[command](from, predicate);
-	    }
-	    else {
-		action += actions[command] + " " + predicate;
-	    }
-	    client.say(to, "\u0001ACTION " + action + "\u0001");
-	}
-
-	// or an acronym?
-	else if (command in acronyms) {
-	    client.say(to, acronyms[command]);
-	}
-
-    }
-});*/
+jerk(function(j){}).connect({ server: server, nick: nick, channels: channels });
